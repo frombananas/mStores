ca642163be6cd5fa747bce26e2900ddeba2898c9
frombananas <kfilippov419@gmail.com>
2026-07-02 13:18:46 +0300

    Update Gradle 8.7 and AndroidX for JVM 21 compat

--- android/app/src/main/java/com/mstores/app/MainActivity.java
+++ android/app/src/main/java/com/mstores/app/MainActivity.java
@@ -1,16 +1,15 @@
 package com.mstores.app;
-import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
-import android.webkit.CookieManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     private WebView webView;
 
     @Override

bbc22d23f77a0c3855403dfc535077b6b3db3dc8
frombananas <kfilippov419@gmail.com>
2026-07-02 14:36:09 +0300

    Add IE11 forced mode for WebBrowser

--- win81/app/MainWindow.xaml.cs
+++ win81/app/MainWindow.xaml.cs
@@ -1,9 +1,25 @@
+using System;
 using System.Windows;
+using Microsoft.Win32;
+
 namespace mStore {
     public partial class MainWindow : Window {
         public MainWindow() {
+            SetBrowserEmulation();
             InitializeComponent();
             browser.Navigate("https://mstores.45.38.143.196.nip.io");
+        }
+
+        static void SetBrowserEmulation() {
+            try {
+                var key = Registry.CurrentUser.OpenSubKey(
+                    @"Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION", true);
+                if (key == null)
+                    key = Registry.CurrentUser.CreateSubKey(
+                        @"Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION");
+                key.SetValue("mStore.exe", 11001, RegistryValueKind.DWord);
+                key.Close();
+            } catch { }
         }
     }
 }

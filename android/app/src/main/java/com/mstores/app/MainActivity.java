f6c94ab41be2fa501a28702b414e2d4128ca6442
frombananas <kfilippov419@gmail.com>
2026-07-03 06:00:35 +0300

    Scale 0.45 + hardware acceleration

--- android/app/src/main/java/com/mstores/app/MainActivity.java
+++ android/app/src/main/java/com/mstores/app/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         hideSystemUI();
         webView = new WebView(this);
+        webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
         setContentView(webView);
 
         WebSettings s = webView.getSettings();
@@ -51,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
                 view.evaluateJavascript("(function(){" +
                     "var m=document.querySelector('meta[name=viewport]');" +
                     "if(!m){m=document.createElement('meta');m.name='viewport';document.head.appendChild(m);}" +
-                    "m.content='width=1440,initial-scale=0.55,user-scalable=yes';" +
+                    "m.content='width=1440,initial-scale=0.45,user-scalable=yes';" +
                     "})()", null);
             }
         });

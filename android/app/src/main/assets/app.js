04d9368380e737fcd769bf5e47e4fdfab072c3c0
frombananas <kfilippov419@gmail.com>
2026-07-04 07:21:40 +0300

    Revert all featured changes back to original

--- android/app/src/main/assets/app.js
+++ android/app/src/main/assets/app.js
@@ -305,7 +305,7 @@
       delay += step;
     });
 
-    var featuredApps = apps.slice(0, 6);
+    var featuredApps = apps.slice(3, 12);
     featuredApps.forEach(function(app){
       var el = createFeaturedTile(app, delay);
       featuredEl.appendChild(el);
